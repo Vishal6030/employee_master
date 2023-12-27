fbeb5a619de7322fa178c8823dd3dd480fbc78ab
Vishal6030 <vishal.sah@bulkbuyindia.com>
2023-12-27 17:09:39 +0530

    Adding new Project

---
+++ src/main/java/com/employee/employee_master/EmployeeMasterApplication.java
@@ -0,0 +1,13 @@
+package com.employee.employee_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMasterApplication.class, args);
+	}
+
+}
